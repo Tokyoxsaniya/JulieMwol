494a26ef488c01b33750820fdd22653b6b4b67ab
Tokyo <87141721+Tokyoxsaniya@users.noreply.github.com>
2021-08-01 09:44:54 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,7 +240,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~🎀______~𝙏𝙊𝙆𝙔𝙊𝘽𝙊𝙏~_____🎀~*' +
+                                await conn.sendMessage(conn.user.jid, '*🦄_______𝙏𝙊𝙆𝙔𝙊𝘽𝙊𝙏______🦄*' +
                                     '\n\n*🧞‍♂️ ' + error + '*\n'
                                     , MessageType.text);
                             }
